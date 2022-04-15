29f9a68153c355bff8313002afbf01ee94fb39df
fachebot <peatdev@gmail.com>
2022-04-15 16:38:19 +0800

    完成获取个人信息接口

--- src/logic/get_me_logic.rs
+++ src/logic/get_me_logic.rs
@@ -1,8 +1,34 @@
 use crate::service::Context;
-use tide::Request;
+use crate::types::{GetMeResponse, Transport};
+use tide::{Body, Request, Response};
 
 pub async fn get_me(req: Request<Context>) -> tide::Result {
-    let address = req.ext::<String>();
-    println!("get_me: {:?}", address);
-    Ok("ok".into())
+    let address = req.ext::<String>().unwrap();
+
+    let user = req
+        .state()
+        .user_model
+        .find_one_by_wallet_address(address)
+        .await?;
+    let transports = req
+        .state()
+        .transport_model
+        .find_all_by_user_id(user.id)
+        .await?;
+
+    let mut res = GetMeResponse {
+        id: user.id,
+        open_id: user.open_id.to_string(),
+        project_id: user.project_id.to_string(),
+        transports: Vec::new(),
+    };
+    for transport in &transports {
+        res.transports.push(Transport {
+            transport_type: transport.transport_type.clone(),
+            chat_id: transport.chat_id.clone(),
+            connected: transport.connected,
+        });
+    }
+
+    Ok(Response::builder(200).body(Body::from_json(&res)?).build())
 }
--- src/types/mod.rs
+++ src/types/mod.rs
@@ -11,3 +11,20 @@ pub struct AuthRequest {
 pub struct AuthResponse {
     pub access_token: String,
 }
+
+#[derive(Debug, Serialize)]
+pub struct Transport {
+    #[serde(rename = "type")]
+    pub transport_type: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub chat_id: Option<String>,
+    pub connected: bool,
+}
+
+#[derive(Debug, Serialize)]
+pub struct GetMeResponse {
+    pub id: i64,
+    pub open_id: String,
+    pub project_id: String,
+    pub transports: Vec<Transport>,
+}
