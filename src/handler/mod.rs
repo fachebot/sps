fa7464461b4107d4a27f9cc7b6542ec6e034bb98
fachebot <peatdev@gmail.com>
2022-04-27 22:18:08 +0800

    允许跨域访问

--- src/handler/mod.rs
+++ src/handler/mod.rs
@@ -4,10 +4,24 @@ use crate::logic;
 use crate::service::Context;
 use anyhow::Result;
 use async_std::sync::Arc;
+use http_types::headers::HeaderValue;
 use middleware::{JsonResponseMiddleware, JwtAuthMiddleware};
+use tide::security::{CorsMiddleware, Origin};
 
 pub fn register_handlers(app: &mut tide::Server<Arc<Context>>) -> Result<()> {
     app.with(JsonResponseMiddleware::new());
+    app.with(
+        CorsMiddleware::new()
+            .allow_methods("GET, POST, OPTIONS".parse::<HeaderValue>().unwrap())
+            .allow_headers(
+                "Authorization, Content-Type"
+                    .parse::<HeaderValue>()
+                    .unwrap(),
+            )
+            .allow_origin(Origin::from("*"))
+            .allow_credentials(false),
+    );
+
     let access_secret = &app.state().conf.server.access_secret;
     let jwt_middleware = JwtAuthMiddleware::new(access_secret)?;
 
